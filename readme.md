d8368161562eeecd166678880f1f36cc9ece1364
TorrentC <1783968112@qq.com>
2019-07-14 08:51:05 +0800

    这是我第一次使用idea git

---
+++ readme.md
@@ -0,0 +1,13 @@
+## this is a git about idea
+
+#### by this way, I can practice my idea git
+**work hard**
+
+~~~~
+
+_print_
+
+`ffffff`
+
+~~~~
+
---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,6 @@
+public class HelloGit {
+    public static void main(String[] args) {
+        User merry = new User(1, "merry");
+        System.out.println(merry);
+    }
+}
