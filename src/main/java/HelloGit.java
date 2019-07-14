701924513faf8c464ecbc602cdfe2f2cefd999e8
TorrentC <1783968112@qq.com>
2019-07-14 09:02:06 +0800

    Play

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         User merry = new User(1, "merry");
         System.out.println(merry);
+        System.out.println("ping");
     }
 }
